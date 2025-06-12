9e534901a32faa19585efe665852db7d15b60877
Justin Mitchel <jmitchel3@users.noreply.github.com>
2025-06-12 15:04:41 -0600

    33 - LangChain with FastAPI and SQLModel

--- backend/src/api/chat/routing.py
+++ backend/src/api/chat/routing.py
@@ -3,6 +3,8 @@ from fastapi import APIRouter, Depends
 from sqlmodel import Session, select
 
 from api.db import get_session
+from api.ai.schemas import EmailMessageSchema
+from api.ai.services import generate_email_message
 from .models import ChatMessagePayload, ChatMessage, ChatMessageListItem
 router = APIRouter()
 
@@ -23,7 +25,9 @@ def chat_list_messages(session: Session = Depends(get_session)):
 # HTTP POST -> payload = {"message": "Hello world"} -> {"message": "hello world", "id": 1}
 # curl -X POST -d '{"message": "Hello world"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
 # curl -X POST -d '{"message": "Hello world"}' -H "Content-Type: application/json"  https://docker-api-fastapi-python-app-3jid9.ondigitalocean.app/api/chats/
-@router.post("/", response_model=ChatMessageListItem)
+
+# curl -X POST -d '{"message": "Give me a summary of why it's good to go outside"}' -H "Content-Type: application/json" http://localhost:8080/api/chats/
+@router.post("/", response_model=EmailMessageSchema)
 def chat_create_message(
     payload:ChatMessagePayload,
     session: Session = Depends(get_session)
@@ -32,6 +36,7 @@ def chat_create_message(
     obj = ChatMessage.model_validate(data)
     session.add(obj)
     session.commit()
-    session.refresh(obj) # ensure id/primary key added to the obj instance
+    # session.refresh(obj) # ensure id/primary key added to the obj instance
     # ready to store in the database
-    return obj
+    response = generate_email_message(payload.message)
+    return response
